74cd03a9b8afcafa4093fab7429abe5fd9ea9539
Francis Field <ffield@udel.edu>
2015-09-25 13:38:08 -0400

    Added PS2

--- PS2/src/MainPackage/MyInteger.java
+++ PS2/src/MainPackage/MyInteger.java
@@ -28,4 +28,140 @@ public class MyInteger {
 			return false;
 		}
 	}
+	
+	public static boolean isEven( int value)
+	{
+		if ( value % 2 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isEven(MyInteger myInt)
+	{
+		if (myInt.getValue() % 2 == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isOdd()
+	{
+		if (this.getValue() % 2 != 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isOdd( int value)
+	{
+		if ( value % 2 != 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isOdd(MyInteger myInt)
+	{
+		if (myInt.getValue() % 2 != 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isPrime()
+	{
+		boolean bool = false;
+		value = this.getValue();
+		int n;
+		for ( n = 2; n <= value;) 
+		{
+			if (value % n == 0)
+			{
+				bool = false;
+			}
+			else
+			{
+				bool = true;
+			}
+		}
+		return bool;
+	}
+	
+	public static boolean isPrime( int value)
+	{
+		boolean bool = false;
+		int n;
+		for ( n = 2; n <= value;) 
+		{
+			if (value % n == 0)
+			{
+				bool = false;
+			}
+			else
+			{
+				bool = true;
+			}
+			
+		}
+		return bool;
+		}
+	
+	public static boolean isPrime(MyInteger myInt)
+	{
+		int value = myInt.getValue();
+		int n;
+		boolean bool = false;
+		for ( n = 2; n <= value;) 
+		{
+			if (value % n == 0)
+			{
+				bool = false;
+			}
+			
+			else
+			{
+				bool = true;
+			}
+		}
+		return bool;
+	}
+	
+	public boolean equals(int integer)
+	{
+		
+		int value1 = integer;
+		if (value1 == integer )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
 }
+	
+	
+
